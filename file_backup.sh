db647ba8eca8b5bd884cf188c2c048a5a1dd7094
Peter <Peter92@users.noreply.github.com>
2018-01-25 04:15:44 +0000

    Create file_backup.sh

---
+++ file_backup.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+#Split a file into multiple parts, compress and email
+#Usage: ./file_backup.sh input_file, [--split-size 10m] [--email user@email.com] [--disable-compression]
+#TODO: Time taken (for email body), maximum_file_limit
+
+source config.conf
+
+mkdir ${TEMPDIR} -p
+
+file_path=$1
+
+compress_file=true
+
+while [ $# -ge 1 ]; do
+        case "$1" in
+                -- )
+                    shift
+                    break
+                   ;;
+                -e | --email )
+                        email_address="$2"
+                        shift
+                        ;;
+                -s | --split-size )
+                        split_size="$2"
+                        shift
+                        ;;
+                -dc | --disable-compression )
+                        compress_file=false
+                        ;;
+                -h | --help )
+                        echo "Display some help"
+                        exit 0
+                        ;;
+        esac
+        shift
+done
+
+base_file=$(basename $file_path)
+
+original_file=${base_file}
+
+#Copy and compress
+if [ ${compress_file} = true ]; then
+    cp ${file_path} ${TEMPDIR}/${original_file}
+    gzip ${TEMPDIR}/${original_file}
+    file_path=${TEMPDIR}/${original_file}.gz
+    original_file=$(basename $file_path)
+fi
+
+#Split the file
+if [ -z ${split_size} ] || [ ${split_size} = false ];
+then
+    cp ${file_path} ${TEMPDIR}/${original_file}.0
+    #file_list[0]=${TEMPDIR}/${original_file}.0
+else
+    split --bytes ${split_size} --numeric-suffixes --suffix-length 1 ${file_path} ${TEMPDIR}/${original_file}.
+    echo find "${TEMPDIR}" -iname "${original_file}.*"
+    #file_list=${find "${TEMPDIR}" -iname "${original_file}.*"}
+fi
+
+#Clean up
+index=1
+num_files=$(find ${TEMPDIR} -iname ${original_file}.* -type f -printf '.' | wc -c)
+for file in ${TEMPDIR}/${original_file}.*
+do  
+    next_index=${index}+1
+    subject="Backup of ${base_file}"
+    if [ ${num_files} -gt 1 ]; then
+        subject="${subject} (Part ${index})"
+    fi
+    message="Test backup"
+    
+    #Send email
+    if [ ! -z ${email_address} ]; then
+        echo "Sending ${file} to ${email_address}... ${index}/${num_files}"
+        #echo "${message}" | mutt -a ${file} -s "${subject}" -e "my_hdr From:${EMAIL}" -- ${email_address}
+    fi
+    
+    #Delete
+    rm $file
+    let "index++"
+done
+
+#Delete compressed file
+if [ ${compress_file} = true ]; then
+    rm ${file_path}
+fi
