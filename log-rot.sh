cd1acc87bdad74f428a418121189bdcd2f99b448
Peter <Peter92@users.noreply.github.com>
2018-01-29 14:25:23 +0000

    Option to combine logs in single file

--- log-rot.sh
+++ log-rot.sh
@@ -2,6 +2,8 @@
 #Email all the rotated logs and optionally delete them
 #Requires logs to be rotated as file.log.*, or file.log.*.gz
 
+(( time_start=$(date +%s%N)/1000000 ))
+
 #Create temp folder - https://stackoverflow.com/a/34676160/2403000
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 work_dir=$(mktemp -d -p "$script_dir")
@@ -21,6 +23,7 @@ compress_file=true
 extension="log"
 delete=false
 sleep_time=1
+combine=false
 while [ $# -ge 1 ]; do
         case "$1" in
                 -- )
@@ -37,6 +40,9 @@ while [ $# -ge 1 ]; do
                 -s | --split | --split-size | --split-file )
                         split_size="$2"
                         shift
+                        ;;
+                -c | --combine )
+                        combine=true
                         ;;
                 --sleep )
                         sleep_time="$2"
@@ -66,6 +72,7 @@ while IFS=  read -r -d $'\0'; do
     rotated_logs+=("$REPLY")
 done < <(find "$log_dir" -name "*.$extension.*" -print0)
 
+file_array=()
 for file in "${rotated_logs[@]}"
 do
     #Get extension and base name from path
@@ -97,23 +104,33 @@ do
     fi
     
     #Compress file and remove timestamp
-    if [ "$compress_file" = true ]; then
+    if [ "$compress_file" = true ] && [ "$combine" = false ]; then
         gzip "$new_path"
-        mv "$new_path.gz" "$work_dir/$new_zip.gz"
-        new_path="$work_dir/$new_zip.gz"
+        final_file="$new_zip.gz"
+        mv "$new_path.gz" "$work_dir/$final_file"
+        new_path="$work_dir/$final_file"
     fi
     
-    #Send to backup script
+    #Send to backup script (or prepare in the case of combining)
     custom_name="$(expr "$file" : "$log_dir/\(.*\)$file_name")$new_zip"
-    echo "Sending $custom_name to backup function..."
     custom_body="$email_body\nFile was saved at $(date --date "$last_modified_raw" +'%Y-%m-%d %H:%M:%S')."
-    ./main.sh "$new_path" --email "$email_address" --split "$split_size" --name-override "$custom_name" --message "$custom_body" --uncompressed
+    file_array+=("$final_file")
+    if [ "$combine" = false ]; then
+        echo "Sending $custom_name to backup function..."
+        ./main.sh "$new_path" --email "$email_address" --split "$split_size" --name-override "$custom_name" --message "$custom_body" --uncompressed
+        sleep "$sleep_time"
+    fi
     
     #Delete original log file
     if [ "$delete" = true ]; then
         rm "$file"
     fi
     
-    sleep "$sleep_time"
-    
 done
+
+#Combine the files before sending
+if [ "$combine" = true ]; then
+    echo "Compressing logs and sending to backup function..."
+    tar -cf "$work_dir/logs.tar" --directory="$work_dir" "${file_array[@]}"
+    ./main.sh "$work_dir/logs.tar" --email "$email_address" --split "$split_size" --name-override "log files" --message "$email_body" --time-start "$time_start"
+fi
